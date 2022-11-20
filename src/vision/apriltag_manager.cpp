3ceff343fc6f17bc2cd009e592c36c54c556e432
deslobodzian <de.slobodzian@gmail.com>
2022-11-19 22:01:17 -0500

    makes sure we are not destorying an object that has not been alocated

--- src/vision/apriltag_manager.cpp
+++ src/vision/apriltag_manager.cpp
@@ -26,25 +26,27 @@ void AprilTagManager::detector_zed(Zed &camera) {
         std::vector<TrackedTargetInfo> targets;
 
         apriltag_detection_t *det;
-        for (int i = 0; i < zed_detector_.get_current_number_of_targets(); i++) {
-            zarray_get(zed_detector_.get_current_detections(), i, &det);
-            Corners c = zed_detector_.get_detection_corners(det);
-            sl::float3 tr = camera.get_position_from_pixel(c.tr);
-            sl::float3 tl = camera.get_position_from_pixel(c.tl);
-            sl::float3 br = camera.get_position_from_pixel(c.br);
-            if (is_vec_nan(tr) || is_vec_nan(tl) || is_vec_nan(br)){
-                error("Vec is nan");
-            } else {
-                sl::Pose pose = zed_detector_.get_estimated_target_pose(tr, tl, br);
-                targets.emplace_back(TrackedTargetInfo(pose, det->id));
-            }
-        }
-        const std::lock_guard<std::mutex> lock(zed_mtx_);
-        zed_targets_ = targets;
-        apriltag_detection_destroy(det);
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-        zed_dt_ = duration.count();
+	if (zed_detector_.has_targets()) {
+        	for (int i = 0; i < zed_detector_.get_current_number_of_targets(); i++) {
+            		zarray_get(zed_detector_.get_current_detections(), i, &det);
+            		Corners c = zed_detector_.get_detection_corners(det);
+            		sl::float3 tr = camera.get_position_from_pixel(c.tr);
+            		sl::float3 tl = camera.get_position_from_pixel(c.tl);
+            		sl::float3 br = camera.get_position_from_pixel(c.br);
+            		if (is_vec_nan(tr) || is_vec_nan(tl) || is_vec_nan(br)){
+                		error("Vec is nan");
+            		} else {
+                		sl::Pose pose = zed_detector_.get_estimated_target_pose(tr, tl, br);
+                		targets.emplace_back(TrackedTargetInfo(pose, det->id));
+            		}
+        	}
+        	const std::lock_guard<std::mutex> lock(zed_mtx_);
+        	zed_targets_ = targets;
+        	apriltag_detection_destroy(det);
+        	auto stop = std::chrono::high_resolution_clock::now();
+        	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+        	zed_dt_ = duration.count();
+	}
 //        info("Zed thread took " + std::to_string(duration.count()) + " milliseconds");
 
     }
