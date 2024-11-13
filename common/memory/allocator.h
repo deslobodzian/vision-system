7c9920fb21eb6e45d67470d76b2c58e21baf37e2
de.slobodzian <de.slobodzian@gmail.com>
2024-11-12 23:56:05 -0500

    Go back to bazel build build system, look to use not ZMQ maybe own library?

---
+++ common/memory/allocator.h
@@ -0,0 +1,15 @@
+#ifndef VISION_SYSTEM_COMMON_ALLOCATOR_H
+#define VISION_SYSTEM_COMMON_ALLOCATOR_H
+
+/*
+ * Virutal class for all allocator types
+ */
+class Allocator {
+public:
+    virtual void* alloc(size_t size) = 0;
+    virtual void free(void* ptr) = 0;
+    virtual int copy_to(void* dst, void* src, size_t size) = 0;
+    virtual int copy_from(void* dst, void* src, size_t size) = 0;
+};
+
+#endif /* VISION_SYSTEM_COMMON_ALLOCATOR_H */
---
+++ tools/formatter/formatter.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+"""
+    Base class for formatters for this project
+"""
+class FormatterBase:
+    def __init__():
+        file_dirs = []
