5224e228a92510a0ef6418d72ddef2545196f1fd
Dennis_Slobodzian <de.slobodzian@gmail.com>
2025-10-07 00:23:18 -0400

    wip

---
+++ common/tests/image_tests.cc
@@ -0,0 +1,16 @@
+#include <image.h>
+#include <logger.h>
+#include <device_manager.h>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("CpuImageTest", "[image]") {
+    auto cpu_dev = DeviceManager::instance().get_device("CPU");
+    LOG_DEBUG("Creating image on device: ", cpu_dev->name());
+    Image<double> image(*cpu_dev, 32, 64, 3);
+
+    REQUIRE(image.height() == 32);
+    REQUIRE(image.width() == 64);
+    REQUIRE(image.channels() == 3);
+    REQUIRE(image.stride() == 64 * 3 * sizeof(double));
+    REQUIRE(image.size() == 32 * 64 * 3 * sizeof(double));
+}
